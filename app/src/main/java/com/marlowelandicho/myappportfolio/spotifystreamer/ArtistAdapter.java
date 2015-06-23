1ec7b6eeaa2bc56ceb8ac913e9dd6e4de9f41f32
Marlowe Landicho <marlowe.landicho@sungard.com>
2015-06-24 00:36:22 +0800

    Task 1 and 2

---
+++ app/src/main/java/com/marlowelandicho/myappportfolio/spotifystreamer/ArtistAdapter.java
@@ -0,0 +1,74 @@
+package com.marlowelandicho.myappportfolio.spotifystreamer;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+import kaaes.spotify.webapi.android.models.Artist;
+
+/**
+ * Created by marlowe.landicho on 24/6/15.
+ */
+public class ArtistAdapter extends BaseAdapter {
+    Context context;
+    List<Artist> artistList;
+
+    public ArtistAdapter(Context context, List<Artist> artistList) {
+        this.context = context;
+        this.artistList = artistList;
+    }
+
+    /*private view holder class*/
+    private class ViewHolder {
+        ImageView imageView;
+        TextView txtTitle;
+        TextView txtDesc;
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder = null;
+
+        LayoutInflater mInflater = (LayoutInflater)
+                context.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+        if (convertView == null) {
+            convertView = mInflater.inflate(R.layout.list_item, null);
+            holder = new ViewHolder();
+            holder.txtDesc = (TextView) convertView.findViewById(R.id.desc);
+            holder.txtTitle = (TextView) convertView.findViewById(R.id.title);
+            holder.imageView = (ImageView) convertView.findViewById(R.id.icon);
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+
+        Artist rowItem = (Artist) getItem(position);
+
+        holder.txtDesc.setText(rowItem.getDesc());
+        holder.txtTitle.setText(rowItem.getTitle());
+        holder.imageView.setImageResource(rowItem.getImageId());
+
+        return convertView;
+    }
+
+    @Override
+    public int getCount() {
+        return rowItems.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return rowItems.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return rowItems.indexOf(getItem(position));
+    }
+}
